5ad7207cf5efa7f91902b9c93219022a7c77f68b
chhatraman shrestha <cms111000111@gmail.com>
2023-05-01 20:50:42 +0545

    farmer changed

--- src/app/authpages/login/login.component.ts
+++ src/app/authpages/login/login.component.ts
@@ -2,11 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { SettingsService } from 'src/app/services/settings.service';
 import { Router } from '@angular/router';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { Subscription } from 'rxjs';
 
 /* eslint-disable eqeqeq */
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { type } from 'os';
+import { Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +17,17 @@ import { type } from 'os';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  phone= '9852078275';
-  password= '9852078275';
+  // phone= '9816985816';
+  // password= '9816985816';
 
   saveLogin = false;
-  // phone = '';
-  // password = '';
+  phone = '';
+  password = '';
+  private backButtonSubscription: Subscription;
 
   constructor(public auth: AuthService, private router: Router,
     private barcodeScanner: BarcodeScanner,
+    private platform: Platform,
     public setting: SettingsService,
     private client: HttpClient) {
     const saveLoginSTR = localStorage.getItem('_xcb_save_login');
@@ -32,13 +37,20 @@ export class LoginComponent implements OnInit {
       localStorage.setItem('_xcb_save_login', this.saveLogin.toString());
     }
 
-    // this.phone=localStorage.getItem('_xcbphone')??'';
+    if(this.saveLogin){
+      this.phone=localStorage.getItem('_xcb_saved_phone')??"";
+    }
+
   }
 
   ngOnInit() {
     this.auth.authend.subscribe((data) => {
-      // this.password = '';
-      this.router.navigate(['/tabs/tab1']);
+      if(this.saveLogin){
+        localStorage.setItem('_xcb_saved_phone', this.phone);
+      }
+      this.router.navigate(['/tabs/tab1'],{
+        replaceUrl:true
+      });
     });
   }
 
@@ -47,11 +59,8 @@ export class LoginComponent implements OnInit {
     //   alert('Please enter phone no');
     //   return;
     // }
-    if(this.saveLogin){
-      this.auth.login(this.phone.toString(), this.phone.toString());
-      return;
-    }
-    if (this.password == undefined) {
+
+    if (this.password == undefined || this.password=="") {
       alert('Please enter password');
       return;
     }
@@ -83,4 +92,25 @@ export class LoginComponent implements OnInit {
     localStorage.setItem('_xcb_save_login', this.saveLogin.toString());
 
   }
+
+  ionViewDidEnter() {
+    this.backButtonSubscription=this.platform.backButton.subscribeWithPriority(10, (next) => {
+        console.log(this.router.url);
+        if(this.router.url=="/login"){
+          if(confirm("Do you want to exit application")){
+            App.exitApp();
+          }
+        }else{
+          next();
+        }
+      }
+    );
+  }
+
+  ionViewDidLeave() {
+    if (this.backButtonSubscription) {
+      this.backButtonSubscription.unsubscribe();
+    }
+  }
+
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,9 @@ import { SettingsService } from 'src/app/services/settings.service';
 import { Component } from '@angular/core';
 import { SqlliteService } from '../services/sqllite.service';
 import { AuthService } from '../services/auth.service';
+import { NavController, Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -11,10 +14,17 @@ import { AuthService } from '../services/auth.service';
 })
 export class Tab1Page {
 
+  private backButtonSubscription: Subscription;
+
   constructor(
     private db: SqlliteService,
     public auth: AuthService,
-    public setting: SettingsService) {}
+    public router: Router,
+    private navCtrl: NavController,
+    private platform: Platform,
+    public setting: SettingsService) {
+
+    }
   del(){
     this.db.run('delete from milkamounts where id>0')
     .then((res)=>{
@@ -23,6 +33,26 @@ export class Tab1Page {
     .catch((err)=>{
       console.log(err);
     });
+  }
+
+  ionViewDidEnter() {
+    this.backButtonSubscription=this.platform.backButton.subscribeWithPriority(10, (next) => {
+        console.log(this.router.url);
+        if(this.router.url=="/tabs/tab1"){
+          if(confirm("Logout From Application")){
+            this.router.navigate(['/login'],{replaceUrl:true});
+          }
+        }else{
+          next();
+        }
+      }
+    );
+  }
+
+  ionViewDidLeave() {
+    if (this.backButtonSubscription) {
+      this.backButtonSubscription.unsubscribe();
+    }
   }
 
   activeParts(datas){
